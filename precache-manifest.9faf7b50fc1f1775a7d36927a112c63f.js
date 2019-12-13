6602c991d0f7c4fb35a6230086fd4d28feff8d32
Zotapianola <zotapianola@gmail.com>
2019-12-13 06:04:03 -0400

    Updates

---
+++ precache-manifest.9faf7b50fc1f1775a7d36927a112c63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef8a5614ba07b3b2c9818e7cd8fc1ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6cadf809568f447df2",
+    "url": "/static/css/main.6ba85ccb.chunk.css"
+  },
+  {
+    "revision": "8fa2874aa372f1369035",
+    "url": "/static/js/2.ac7fbbea.chunk.js"
+  },
+  {
+    "revision": "ac6cadf809568f447df2",
+    "url": "/static/js/main.87083bb8.chunk.js"
+  },
+  {
+    "revision": "06342275598567d615bf",
+    "url": "/static/js/runtime-main.c9607c74.js"
+  }
+]);
